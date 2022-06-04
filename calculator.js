5fd0b478c95fe5c396273b751dfa26b77635647d
VladScv <ddcllrnsbrv@uoc.edu>
2022-06-04 03:33:11 +0200

    some Js playing

--- calculator.js
+++ calculator.js
@@ -1,12 +1,56 @@
 /*------------------------------------------------------------------ELEMENTS SELECTION */
-const calculator = document.querySelector('.calculator')
-const consoleOut = document.querySelector('.console')
-const screen= {firstLine: document.querySelector('#first-line'), mainLine:document.querySelector('#main-line')}
-// const numberPad=
+const calculator = document.querySelector('.calculator');
+const consoleOut = document.querySelector('.console');
+const screen= {
+    firstLine: document.querySelector('#first-line'),
+    mainLine:document.querySelector('#main-line')
+};
+// let numberPad = document.querySelectorAll('.num-btns')
+const numberPad=[];
+for (let i=0; i<10;i++){
+    let str='#\\3'+i+'-btn'
+    numberPad.push(document.querySelector(str));
+    numberPad[i].addEventListener('click',()=>{outputMessage(i+' pressed')});
+}
 
+/*------------------------------------------------------------------ INPUT ASSIGNMENT */
+document.addEventListener("keydown",function (e){
+    e.preventDefault();
+    switch (e.key){
+        case 'Enter': outputMessage('Enter press!')
+
+            break
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            const num= parseInt(e.key,10);
+            numberPad[num].className='numpress';
+            numberPad[num].click();
+            setTimeout(()=>{
+
+                numberPad[num].className='button';
+            },300)
+            break
+        default:
+            break
+    }
+})
+/*----------------------------------------------------------------------------------FUNCTIONS */
 const outputMessage= function(str) {
     const mssg= `</br> &gt; ${str}`
     consoleOut.insertAdjacentHTML("beforeend", mssg);
-    consoleOut.scroll(0,consoleOut.scrollHeight)
+    consoleOut.scroll(0,consoleOut.scrollHeight);
 }
-calculator.addEventListener('click',()=>{outputMessage('hi!')},false)
+
+
+
+
+/*----------------------------------------------------------------------------------LISTENERS */
+// calculator.addEventListener('click',()=>{outputMessage('hi!')},false)
